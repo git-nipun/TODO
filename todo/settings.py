4eb4050fb34c0e39976b9c24717d752bab26f5df
Nipun <nipungautam95@gmail.com>
2022-08-19 20:17:22 +0530

    SECRET_KEY

--- todo/settings.py
+++ todo/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5px$iu4ej+=*c==+w+d(ctl+s^vj#diwc@w#*wssrz!zze5fma'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["mytodo-22.herokuapp.com","localhost"]
 
